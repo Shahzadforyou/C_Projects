27a50b50e9f2aa669d6b98ed966002ee5324dd44
Shahzadforyou <zaeimshahzad@gmail.com>
2025-06-27 15:45:05 +0500

    Making struct Student to store data of student in vector student

---
+++ student_record_form.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+struct Student{
+    string name;
+    int rollno;
+    float cgpa;
+};
+int main(){
+    vector <Student> student;
+    return 0;
+}
