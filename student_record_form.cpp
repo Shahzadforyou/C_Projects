08b6623f19902891653e6b0109f636061cccf250
Shahzadforyou <zaeimshahzad@gmail.com>
2025-06-27 15:55:14 +0500

    making a function to add student in to data list of students

--- student_record_form.cpp
+++ student_record_form.cpp
@@ -7,6 +7,16 @@ struct Student{
     int rollno;
     float cgpa;
 };
+void add_Student(vector <Student>&student){
+    Student s;
+    cout<<"Enter the name of student"<<endl;
+    cin>>s.name;
+    cout<<"Enter the roll number of student"<<endl;
+    cin>>s.rollno;
+    cout<<"Enter the cgpa of student"<<endl;
+    cin>>s.cgpa;
+    student.push_back(s);
+}
 int main(){
     vector <Student> student;
     return 0;
