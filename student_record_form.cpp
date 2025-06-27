9a17937924788f723a8983f3a2c3861efeb2c67c
Shahzadforyou <zaeimshahzad@gmail.com>
2025-06-27 17:26:03 +0500

    Making display function that receive constant  vector made for student and display data in it using for loop

--- student_record_form.cpp
+++ student_record_form.cpp
@@ -17,7 +17,14 @@ void add_Student(vector <Student>&student){
     cin>>s.cgpa;
     student.push_back(s);
 }
+void display_student(const vector <Student> & student){
+    cout<<"Student Records"<<endl;
+    for(const auto& s : student){
+        cout<<"Name is "<<s.name<<endl<<"Roll number is "<<s.rollno<<"CGPA is "<<s.cgpa<<endl;
+    }
+};
 int main(){
     vector <Student> student;
+
     return 0;
 }
